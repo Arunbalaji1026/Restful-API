299f7894c8015df6cedd7bdbae3d9981285edc72
Arunbalaji1026 <arunbala94@gmail.com>
2020-06-17 11:12:54 +0530

    first commit

---
+++ views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import employee
+from .serializers import employeeSerializer
+
+class indexList(APIView):
+
+    def get(self, request, format=None):
+        employe_info = employee.objects.all()
+        serializer = employeeSerializer(employe_info, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
